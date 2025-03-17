ded9138774b7b17abf86ca2b08333b3656d74e67
anderson <anderson.andres.dev@gmail.com>
2025-03-17 15:55:16 -0500

    Se agrego conexion a base de datos, con test

--- public/index.php
+++ public/index.php
@@ -3,8 +3,14 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 use App\Process\Classes\Hello;
+use App\Process\BD\Connection;
 
 require __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+//$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+
+$dotenv->load();
 
 $app = AppFactory::create();
 $app->setBasePath('/process.api.test');
@@ -16,6 +22,30 @@ $app->get('/hello/{nombre}', function (Request $request, Response $response, arr
     $json = $hello->saludar(nombre: $args['nombre']);
 
     $response->getBody()->write($json);
+    return $response->withHeader('Content-Type', 'application/json');
+});
+
+$app->get('/test-db', function (Request $request, Response $response) {
+    try {
+        $db = new Connection();
+        $pdo = $db->getConnection();
+
+        // Obtener datos relevantes
+        $stmt = $pdo->query("SELECT DATABASE() as db_name");
+        $data = $stmt->fetch();
+
+        $result = [
+            'status' => 'success',
+            'database_name' => $data['db_name'] ?? 'Unknown'
+        ];
+    } catch (PDOException $e) {
+        $result = [
+            'status' => 'error',
+            'message' => $e->getMessage()
+        ];
+    }
+
+    $response->getBody()->write(json_encode($result));
     return $response->withHeader('Content-Type', 'application/json');
 });
 
